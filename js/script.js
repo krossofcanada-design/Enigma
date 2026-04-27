73bdd44a4dde9728c6ff32c9e7f55fe5784a870b
“KrossIBA” <klaran01@iba.dk>
2026-04-27 05:28:36 -0300

    Stage and answer setup in Figma and html

--- js/script.js
+++ js/script.js
@@ -2,14 +2,39 @@
 
 const stages = document.querySelectorAll(".stage");
 const btns = document.querySelectorAll(".btn");
+const answers = [
+    document.querySelector(".answer1"),
+    document.querySelector(".answer2"),
+    document.querySelector(".answer3"),
+    document.querySelector(".answer4")
+];
 
 
 
-//Functions
+//Functions - Building the branch tree!
 
-//Building the branch tree!
+// One non-redundant helper that resets each stage and annswer
+const resetUI = () => {
+    stages.forEach(stage => stage.classList.remove("active"));
+    answers.forEach(answer => answer.classList.remove("active"));
+};
 
-const nextStage = (e) => {
+
+const showStage1WithAnswers = () => {
+    resetUI();
+
+    // Show stage 1
+    stages[1].classList.add("active");
+
+    // Show all answer areas
+    answers[0].classList.add("active");
+    answers[1].classList.add("active");
+    answers[2].classList.add("active");
+    answers[3].classList.add("active");
+};
+
+
+/* const nextStage = (e) => {
     // console.log("clicked")
     console.log(e.target.textContent); //knapper skal have unik tekst (textContent)
     switch(e.target.textContent) {
@@ -18,7 +43,7 @@ const nextStage = (e) => {
             stages[0].classList.remove("active");
             btns[0].classList.remove("active");
             stages[1].classList.add("active");
-            btns[1].classList.add("active")
+            btns[1].classList.add("active");
         break;
         case "1) You investigate the students' social media to find more personal information_":
             console.log("Option 1");
@@ -35,8 +60,8 @@ const nextStage = (e) => {
             stages[1].classList.remove("active");
             stages[4].classList.add("active");
         break;
-        case "Start over":
-            console.log("Start over");
+        case "Go back one":
+            console.log("Go back one");
             stages[2].classList.remove("active");
             stages[3].classList.remove("active");
             stages[4].classList.remove("active");
@@ -47,10 +72,28 @@ const nextStage = (e) => {
     }
 }
 
+*/
+
 
 
 // Event listeners
 
+/*
 for (const btn of btns) {
     btn.addEventListener("click", nextStage);
 }
+*/
+
+const nextStage = (e) => {
+    const text = e.currentTarget.textContent.trim();
+
+    switch(text) {
+        case "Continue_":
+            showStage1WithAnswers();
+            break;
+    }
+};
+
+btns.forEach(btn => {
+    btn.addEventListener("click", nextStage);
+});
